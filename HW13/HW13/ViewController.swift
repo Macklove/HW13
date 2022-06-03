424bbdc54d3f96ca8586496bffe6a4e3d6c11891
Makulov Evgeny <makulovev@gmail.com>
2022-06-03 19:04:04 +0300

    Add toggle, struct and enum

--- HW13/HW13/ViewController.swift
+++ HW13/HW13/ViewController.swift
@@ -7,6 +7,32 @@
 
 import UIKit
 
+//MARK: - Structs
+
+struct Section {
+    let options: [SettingsOptionsType]
+}
+
+enum SettingsOptionsType {
+    case staticCell(model: SettingsOptions)
+    case switchCell(model: SettingsSwitchOption)
+}
+struct SettingsSwitchOption {
+    let title: String
+    let icon: UIImage?
+    let iconColor: UIColor
+    let handler: (() -> Void)
+    let isToggled: Bool
+}
+
+struct SettingsOptions {
+    let title: String
+    let detail: String?
+    let icon: UIImage?
+    let iconColor: UIColor
+    let handler: (() -> Void)
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
